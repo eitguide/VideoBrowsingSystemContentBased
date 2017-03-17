6e4555f430c1aec22523d71cddf0980d0cca1354
eitguide <eitguide.np@gmail.com>
2017-03-17 14:18:05 +0700

    Create CustomPictureBox

---
+++ VideoBrowsingSystemContentBased/Widget/CustomPictureBox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VideoBrowsingSystemContentBased.Widget
+{
+    public class CustomPictureBox: PictureBox
+    {
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            ControlPaint.DrawBorder(e.Graphics, e.ClipRectangle, Color.Red, ButtonBorderStyle.Solid);
+        }
+    }
+  
+}
