25800670d3aad523b5a2487285de02c6b4c90c69
eitguide <eitguide.np@gmail.com>
2017-03-21 21:23:03 -0500

    Update FormIndexing

---
+++ VideoBrowsingSystemContentBased/View/IndexProcessing.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace VideoBrowsingSystemContentBased.View
+{
+    partial class IndexProcessing
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTextSpotIndexing = new System.Windows.Forms.Button();
+            this.btnTextCaptionIndexing = new System.Windows.Forms.Button();
+            this.fdjhfjkd = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnParseXML = new System.Windows.Forms.Button();
+            this.btnParseJson = new System.Windows.Forms.Button();
+            this.pbTest = new System.Windows.Forms.ProgressBar();
+            this.btnDoWork = new System.Windows.Forms.Button();
+            this.btnSearchTextSpot = new System.Windows.Forms.Button();
+            this.btnSearchCap = new System.Windows.Forms.Button();
+            this.txtQuery = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnTextSpotIndexing
+            // 
+            this.btnTextSpotIndexing.Location = new System.Drawing.Point(12, 99);
+            this.btnTextSpotIndexing.Name = "btnTextSpotIndexing";
+            this.btnTextSpotIndexing.Size = new System.Drawing.Size(110, 23);
+            this.btnTextSpotIndexing.TabIndex = 0;
+            this.btnTextSpotIndexing.Text = "TextSpot Indexing";
+            this.btnTextSpotIndexing.UseVisualStyleBackColor = true;
+            // 
+            // btnTextCaptionIndexing
+            // 
+            this.btnTextCaptionIndexing.Location = new System.Drawing.Point(145, 99);
+            this.btnTextCaptionIndexing.Name = "btnTextCaptionIndexing";
+            this.btnTextCaptionIndexing.Size = new System.Drawing.Size(133, 23);
+            this.btnTextCaptionIndexing.TabIndex = 1;
+            this.btnTextCaptionIndexing.Text = "TextCaptionIndexig";
+            this.btnTextCaptionIndexing.UseVisualStyleBackColor = true;
+            // 
+            // fdjhfjkd
+            // 
+            this.fdjhfjkd.Location = new System.Drawing.Point(29, 178);
+            this.fdjhfjkd.Name = "fdjhfjkd";
+            this.fdjhfjkd.Size = new System.Drawing.Size(40, 23);
+            this.fdjhfjkd.TabIndex = 2;
+            this.fdjhfjkd.Text = "Status";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Location = new System.Drawing.Point(75, 178);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(203, 23);
+            this.lblStatus.TabIndex = 3;
+            // 
+            // btnParseXML
+            // 
+            this.btnParseXML.Location = new System.Drawing.Point(12, 45);
+            this.btnParseXML.Name = "btnParseXML";
+            this.btnParseXML.Size = new System.Drawing.Size(110, 23);
+            this.btnParseXML.TabIndex = 4;
+            this.btnParseXML.Text = "ParseXML";
+            this.btnParseXML.UseVisualStyleBackColor = true;
+            // 
+            // btnParseJson
+            // 
+            this.btnParseJson.Location = new System.Drawing.Point(145, 45);
+            this.btnParseJson.Name = "btnParseJson";
+            this.btnParseJson.Size = new System.Drawing.Size(133, 23);
+            this.btnParseJson.TabIndex = 5;
+            this.btnParseJson.Text = "Parse Json DenseCap";
+            this.btnParseJson.UseVisualStyleBackColor = true;
+            // 
+            // pbTest
+            // 
+            this.pbTest.Location = new System.Drawing.Point(12, 143);
+            this.pbTest.Name = "pbTest";
+            this.pbTest.Size = new System.Drawing.Size(266, 23);
+            this.pbTest.TabIndex = 6;
+            // 
+            // btnDoWork
+            // 
+            this.btnDoWork.Location = new System.Drawing.Point(284, 143);
+            this.btnDoWork.Name = "btnDoWork";
+            this.btnDoWork.Size = new System.Drawing.Size(65, 23);
+            this.btnDoWork.TabIndex = 7;
+            this.btnDoWork.Text = "DoWork";
+            this.btnDoWork.UseVisualStyleBackColor = true;
+            this.btnDoWork.Click += new System.EventHandler(this.btnDoWork_Click);
+            // 
+            // btnSearchTextSpot
+            // 
+            this.btnSearchTextSpot.Location = new System.Drawing.Point(325, 45);
+            this.btnSearchTextSpot.Name = "btnSearchTextSpot";
+            this.btnSearchTextSpot.Size = new System.Drawing.Size(75, 23);
+            this.btnSearchTextSpot.TabIndex = 8;
+            this.btnSearchTextSpot.Text = "Search TS";
+            this.btnSearchTextSpot.UseVisualStyleBackColor = true;
+            this.btnSearchTextSpot.Click += new System.EventHandler(this.btnSearchTextSpot_Click);
+            // 
+            // btnSearchCap
+            // 
+            this.btnSearchCap.Location = new System.Drawing.Point(325, 98);
+            this.btnSearchCap.Name = "btnSearchCap";
+            this.btnSearchCap.Size = new System.Drawing.Size(75, 23);
+            this.btnSearchCap.TabIndex = 9;
+            this.btnSearchCap.Text = "Search Cap";
+            this.btnSearchCap.UseVisualStyleBackColor = true;
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.Location = new System.Drawing.Point(38, 12);
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.Size = new System.Drawing.Size(362, 20);
+            this.txtQuery.TabIndex = 10;
+            // 
+            // IndexProcessing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 210);
+            this.Controls.Add(this.txtQuery);
+            this.Controls.Add(this.btnSearchCap);
+            this.Controls.Add(this.btnSearchTextSpot);
+            this.Controls.Add(this.btnDoWork);
+            this.Controls.Add(this.pbTest);
+            this.Controls.Add(this.btnParseJson);
+            this.Controls.Add(this.btnParseXML);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.fdjhfjkd);
+            this.Controls.Add(this.btnTextCaptionIndexing);
+            this.Controls.Add(this.btnTextSpotIndexing);
+            this.Name = "IndexProcessing";
+            this.Text = "IndexProcessing";
+            this.Load += new System.EventHandler(this.IndexProcessing_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTextSpotIndexing;
+        private System.Windows.Forms.Button btnTextCaptionIndexing;
+        private System.Windows.Forms.Label fdjhfjkd;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnParseXML;
+        private System.Windows.Forms.Button btnParseJson;
+        private System.Windows.Forms.ProgressBar pbTest;
+        private System.Windows.Forms.Button btnDoWork;
+        private System.Windows.Forms.Button btnSearchTextSpot;
+        private System.Windows.Forms.Button btnSearchCap;
+        private System.Windows.Forms.TextBox txtQuery;
+    }
+}
---
+++ VideoBrowsingSystemContentBased/View/IndexProcessing.cs
@@ -0,0 +1,149 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VideoBrowsingSystemContentBased.Controller;
+using VideoBrowsingSystemContentBased.Controller.TextIndexing;
+using VideoBrowsingSystemContentBased.Model;
+using VideoBrowsingSystemContentBased.TextIndexing;
+
+namespace VideoBrowsingSystemContentBased.View
+{
+    public partial class IndexProcessing : Form
+    {
+
+        BackgroundWorker bk;
+        private IndexStorage textSpotIndexStorage;
+        private IndexStorage textCaptionStorage;
+        public IndexProcessing()
+        {
+            InitializeComponent();
+            btnTextCaptionIndexing.Click += btnTextCaptionIndexing_Click;
+            btnTextSpotIndexing.Click += btnTextSpotIndexing_Click;
+            btnParseXML.Click += btnParseXML_Click;
+            btnParseJson.Click += btnParseJson_Click;
+            btnSearchCap.Click += btnSearchCap_Click;
+
+            textCaptionStorage = new IndexStorage(Config.CAPTION_INDEX_STORAGE);
+            textCaptionStorage.OpenIndexStore();
+
+
+            textSpotIndexStorage = new IndexStorage(Config.TEXTSPOTTING_INDEX_STORAGE);
+            textSpotIndexStorage.OpenIndexStore();
+        }
+
+        void btnSearchCap_Click(object sender, EventArgs e)
+        {
+            List<Object> result = Searching.SearchByQuery(textCaptionStorage, Config.TOP_RANK, txtQuery.Text, SearchType.CAPTION);
+            if(result != null && result.Count >  0)
+            {
+                foreach(TextCaption c in result){
+                    Console.WriteLine(c.FrameName + "\t" + c.Caption);
+                }
+            }
+        }
+
+        void btnParseJson_Click(object sender, EventArgs e)
+        {
+            FileManager.GetInstance().DecodeTextCaptionFromDencap(Config.JSON_DENSECAP_FOLDER_PATH, Config.TEXT_CAPTION_PATH);
+        }
+
+        void btnParseXML_Click(object sender, EventArgs e)
+        {
+            List<TextSpot> textSpot = XMLParser.ProcessingXmlData(Config.XML_FOLDER_PATH);
+            FileManager.GetInstance().SeriablizeObjectToJson(textSpot, Config.TEXT_PLOTTING_PATH);
+        }
+
+        void btnTextSpotIndexing_Click(object sender, EventArgs e)
+        {
+            btnTextSpotIndexing.Enabled = false;
+            List<TextSpot> textSpot = FileManager.GetInstance().DeserializeJson(Config.TEXT_PLOTTING_PATH);
+            IndexStorage indexStorage = new IndexStorage(Config.TEXTSPOTTING_INDEX_STORAGE);
+            indexStorage.OpenIndexStore();
+
+            Indexing.IndexFromDatabaseStorage(indexStorage, textSpot);
+
+            indexStorage.CloseIndexStorage();
+
+            btnTextSpotIndexing.Enabled = true;  
+        }
+
+        void btnTextCaptionIndexing_Click(object sender, EventArgs e)
+        {
+            btnTextCaptionIndexing.Enabled = false;
+
+            lblStatus.Text = "Convert Data Json to List TextCaption";
+            List<TextCaption> caption = JsonConvert.DeserializeObject<List<TextCaption>>(FileManager.GetInstance().ReadContentFile(Config.TEXT_CAPTION_PATH));
+            Console.WriteLine(caption.Count);
+
+            lblStatus.Text = "Create IndexStorage";
+            IndexStorage indexStorage = new IndexStorage(Config.CAPTION_INDEX_STORAGE);
+            indexStorage.OpenIndexStore();
+
+            lblStatus.Text = "Indexing....";
+            Indexing.IndexFromDatabaseStorage(indexStorage, caption);
+
+            lblStatus.Text = "Close IndexStorage";
+            indexStorage.CloseIndexStorage();
+
+    
+            btnTextCaptionIndexing.Enabled = true;
+
+           
+        }
+
+        private void IndexProcessing_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnDoWork_Click(object sender, EventArgs e)
+        {
+            pbTest.Maximum = 10000;
+           bk= new BackgroundWorker();
+            bk.WorkerReportsProgress = true;
+            bk.WorkerSupportsCancellation = true;
+            bk.DoWork += bk_DoWork;
+            bk.ProgressChanged += bk_ProgressChanged;
+            bk.RunWorkerCompleted += bk_RunWorkerCompleted;
+
+            bk.RunWorkerAsync();
+        }
+
+        void bk_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            lblStatus.Text = "Suscess";
+        }
+
+        void bk_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pbTest.Value = e.ProgressPercentage;
+        }
+
+        void bk_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                bk.ReportProgress(i);
+            }
+        }
+
+        private void btnSearchTextSpot_Click(object sender, EventArgs e)
+        {
+            List<Object> result = Searching.SearchByQuery(textSpotIndexStorage, Config.TOP_RANK, txtQuery.Text, SearchType.ORC);
+            if (result != null && result.Count > 0)
+            {
+                foreach (TextSpot c in result)
+                {
+                    Console.WriteLine(c.FileName + "\t" + c.Text);
+                }
+            }
+        }
+    }
+}
